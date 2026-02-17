0f0fc80408d27e5943bdfd50e22d09b806ea1ffd
fani-01 <chintaphanithkumar@gmail.com>
2026-02-17 11:37:35 +0530

    Update translang.py

--- translang.py
+++ translang.py
@@ -53,7 +53,7 @@ if not api_key:
 genai.configure(api_key=api_key)
 
 # ---------------- MODEL ----------------
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 # ---------------- FUNCTION ----------------
 def translate_text(text, source_language, target_language):
