87c980375c10a43490f3e5b087080ce23906d5df
fani-01 <chintaphanithkumar@gmail.com>
2026-02-17 10:02:15 +0530

    Update translang.py

--- translang.py
+++ translang.py
@@ -1,1 +1,17 @@
+from dotenv import load_dotenv
+import streamlit as st
+import os
+import google.generativeai as genai
+
+# Load environment variables
+load_dotenv()
+
+# Get API key from .env file
+api_key = os.getenv("GOOGLE_API_KEY")
+
+# Configure Gemini
+genai.configure(api_key=api_key)
+
+# Initialize model
+model = genai.GenerativeModel("gemini-1.5-flash")
 
